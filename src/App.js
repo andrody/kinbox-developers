fe29a1a8e81d67b92040bbffd4c904f984c03541
Andrew Feitosa <andrewcsz@gmail.com>
2021-07-27 16:47:22 -0300

    spec.yaml

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+
+import { RedocStandalone } from "redoc"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <RedocStandalone specUrl="https://redocly.github.io/redoc/openapi.yaml" />
+        </div>
+    )
 }
 
-export default App;
+export default App
