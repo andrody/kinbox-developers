6dc1613d93aa0e275516013425c9a07aa4fa74cb
Andrew Feitosa <andrewcsz@gmail.com>
2021-07-27 16:52:17 -0300

    customizations

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import { RedocStandalone } from "redoc"
 function App() {
     return (
         <div className="App">
-            <RedocStandalone specUrl="https://redocly.github.io/redoc/openapi.yaml" />
+            <RedocStandalone
+                specUrl="https://raw.githubusercontent.com/kinboxapp/kinbox-developers/master/spec.yaml"
+                options={{
+                    nativeScrollbars: true,
+                    theme: { colors: { primary: { main: "#316fea" } } },
+                }}
+            />
         </div>
     )
 }
